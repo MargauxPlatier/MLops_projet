e0bc7ee12fb3a009216b2f499b9d0eafcaf14d88
MargauxPlatier <margaux.platier@orange.fr>
2024-12-16 14:15:55 +0100

    Modif fonctions

--- src/fonctions.py
+++ src/fonctions.py
@@ -19,3 +19,16 @@ def split_data(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=24)
     return X_train, X_test, y_train, y_test
 
+def cv_scoring(estimator, X, y):
+    """Fonction de scoring pour la validation croisée"""
+    return accuracy_score(y, estimator.predict(X))
+
+def cross_validate_models(models, X, y):
+    """Évalue les modèles avec une validation croisée k-fold"""
+    for model_name, model in models.items():
+        scores = cross_val_score(model, X, y, cv=10, n_jobs=-1, scoring=cv_scoring)
+        print("="*60)
+        print(f"{model_name}")
+        print(f"Scores: {scores}")
+        print(f"Mean Score: {np.mean(scores)}")
+
